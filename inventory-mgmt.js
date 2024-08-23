133e2c25b77d1ce947ebe0d6d3e6c4562420de0a
ravitmg <tmgrawv@gmail.com>
2024-08-23 16:45:13 +0545

    add stock replenishment functionality

--- inventory-mgmt.js
+++ inventory-mgmt.js
@@ -5,12 +5,16 @@ const products = [
         price: 100,
         category: "laptop",
         quantity: 10,
+        threshold: 5,
+        replenishAmount: 10,
     },
     {
         name: "IPhone",
         price: 50,
         category: "Phone",
         quantity: 5,
+        threshold: 1,
+        replenishAmount: 5,
     },
 ];
 
@@ -21,15 +25,18 @@ function purchaseProduct (productName, numberPurchased) {
     if (!productToPurchase) {
         return "Product not found Bro";
     }
-    const newQuantity = productToPurchase.quantity - numberPurchased;
+    const newQuantity = productToPurchase.quantity - numberPurchased; // 4 after purchase
     if (newQuantity < 0) {
         return "We dont have enough on stock Bro";
     }
-    productToPurchase.quantity = newQuantity;
+    if (newQuantity <= productToPurchase.threshold) {
+        productToPurchase.quantity 
+        = newQuantity + productToPurchase.replenishAmount // 4+10 = 14
+    }
     return "Product purchased successfully Bro";
 }
 
-const message = purchaseProduct("Macbook", 5);
+const message = purchaseProduct("Macbook", 6);
 console.log(message);
 console.log(products);
 
